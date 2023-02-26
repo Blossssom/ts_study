817a1f799e459a0c3fa3d6dbd32102373bf910b1
Blossssom <eju2003@gmail.com>
2023-02-27 01:33:58 +0900

    [ADD] add ts study files

--- basics-10-literal-types/app.ts
+++ basics-10-literal-types/app.ts
@@ -1,26 +1,15 @@
-const add = (num1: number, num2: number) => {
-   return num1 + num2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Bloxxom';
+
+if(typeof userInput === 'string') {
+  userName = userInput;
+}
+
+const generateError = (message: string, code: number): never => {
+  throw { message: message, code: code };
 };
 
-const printFunction = (result: string): void | string => {
-  console.log('inner printFunction :', result);
-  return result;
-};
-
-const addHandler = (n1: number, n2: number, callback: (num: number) => void) => {
-  const result = n1 + n2;
-  callback(result);
-};
-
-let combinvalues: (v1: number, v2: number) => number;
-
-combinvalues = add;
-
-console.log(combinvalues(10, 52));
-
-const voidTest = addHandler(30, 20, (res) => {
-  // console.log('inner addHandler :', res);
-  return res;
-});
-
-console.log('inner addHander :', voidTest);
+generateError('is throw error', 500);
