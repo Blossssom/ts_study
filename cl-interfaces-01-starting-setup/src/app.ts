d1b8eff6f9256c724fd6c215245067823401db88
bloxxom <eju2003@gmail.com>
2023-04-08 17:33:00 +0900

    [ADD] add constructor class

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-    name: string;
-    employees: string[] = [];
+    private readonly id: string;
+    readonly name: string;
+    private employees: string[] = [];
 
-    constructor(n: string) {
+    constructor(n: string, id: string) {
         this.name = n;
+        this.id = id;
     }
 
     describe(this: Department) {
-        console.log('describe :', this.name);
+        console.log('describe :', this.name, this.id);
     }
 
     addEmploy(this: Department, employee: string) {
@@ -17,6 +19,38 @@ class Department {
     printEmployee() {
         console.log('is employ :', this.employees);
     }
+};
+
+class ITpart extends Department {
+    admin: string [] = [];
+    constructor(id: string, admin: string[]) {
+        super('blossom', id);
+        this.admin = admin;
+    }
 }
 
-const accounting = new Department('bloxxom');
+class AccountingPart extends Department {
+    reports: string[] = [];
+    constructor(id: string) {
+        super('x_x', id);
+
+    }
+
+    addReports(report: string) {
+        this.reports.push(report);
+    }
+}
+
+const it = new ITpart('itttt', ['bloxxom', 'flower']);
+const account = new AccountingPart('account');
+
+account.addReports('monthReport');
+account.addReports('yearReport');
+
+it.addEmploy('flower');
+
+it.printEmployee();
+it.describe();
+
+console.log(it);
+console.log(account)
