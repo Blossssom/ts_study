21bd4ab8f68749e3c764d25be4ccccdcc5ea9ed6
bloxxom <eju2003@gmail.com>
2023-04-08 15:33:41 +0900

    [ADD] add class part

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,11 +1,22 @@
 class Department {
     name: string;
+    employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
     }
+
+    describe(this: Department) {
+        console.log('describe :', this.name);
+    }
+
+    addEmploy(this: Department, employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployee() {
+        console.log('is employ :', this.employees);
+    }
 }
 
 const accounting = new Department('bloxxom');
-
-console.log(accounting);
